6a3f1e96eab895af44da2fc12478670eebed31e2
raigal-r <105627233+raigal-r@users.noreply.github.com>
2023-07-08 11:56:02 +0000

    proposal listener

--- smartcontracts/deploy/get-proposal.ts
+++ smartcontracts/deploy/get-proposal.ts
@@ -3,35 +3,38 @@ import * as ethers from "ethers";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
 
-// An example of a deploy script that will deploy and call a simple contract.
-export default async function (hre: HardhatRuntimeEnvironment) {
+//export default async function (hre: HardhatRuntimeEnvironment) 
+async function listenToEvents() {
   const provider = new Provider("https://testnet.era.zksync.dev");
-  // Private key of the account used to deploy
-  const wallet = new Wallet("0xcc1149669c0f288ae228c316d93a8fd39b766a7d5ac3f41054f74e4ee7f1a09c").connect(provider);
+
+  //const factoryArtifact = await hre.artifacts.readArtifact("PeerLocal");
+  //console.log(factoryArtifact.abi[16].inputs);
+
+  // Get the contract instance you want to listen to events from
+  const contractAddress = "0xe9567E29E59F103Bd89c40DDB1E95603231bed6b"; // Replace with the actual contract address
+  const contractInterface = new ethers.utils.Interface([
+    'event ProposalCreated(uint256 proposalId, address proposer, address[] targets, uint256[] values, string[] signatures, bytes[] calldatas, uint256 voteStart, uint256 voteEnd, string description)', // Replace with the actual event definition
   
-  const factoryArtifact = await hre.artifacts.readArtifact("PeerLocal");
-  
+  ]);
 
-  console.log(factoryArtifact.abi[16].inputs);
+  // Create a filter to listen to events from the contract
+  const filter = {
+    address: contractAddress,
+    topics: [contractInterface.getEventTopic('ProposalCreated')], // Get the event topic using ethers.js
+  };
 
-  const aaFactory = new ethers.Contract(
-    '0x0510b43988c09035796BB88Ea0B1E4C25f090464',
-    factoryArtifact.abi,
-    wallet
-  );
-  
-    const communities = await aaFactory.offers(0, 1);
-    
-    console.log(communities)
-    
-    const random = Wallet.createRandom();
-    console.log(random);
-    console.log(await random._signingKey());
-
-
-  // const tx = await aaFactory.createCommunity(
-  //     'my community hash',
-  //     250
-  // );
-  // await tx.wait();
+  // Subscribe to events using the filter
+  provider.on(filter, (log) => {
+    console.log('Received event:', log);
+    // Process the event data here
+  });
 }
+
+export default async function main() {
+  try {
+    await listenToEvents();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
