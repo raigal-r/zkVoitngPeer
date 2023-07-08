60c298c0993ff3e045c7caac2d1871bf96e88b70
grmkris <kristjan.grm1@gmail.com>
2023-07-08 18:15:46 +0200

    graphql

--- frontend/src/hooks/useCreateProposal.tsx
+++ frontend/src/hooks/useCreateProposal.tsx
@@ -1,16 +1,13 @@
 import { useEthersSigner } from "../useEthersSigner";
-import { useMutation } from "@tanstack/react-query";
-import { Governor__factory } from "../types";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { ERC20__factory, Governor__factory } from "../types";
+import { Address } from "viem";
+import { BigNumber } from "ethers";
 
-const PROPOSALS = {
-  VoteForSendingMoney: {
-    values: [],
-    calldatas: [],
-    targets: [],
-  },
+const PROPOSAL_DATA = {
+  values: [0],
+  targets: ["0xB8f59AF10d8C66b92d0539C898854040147f8cFf"],
 };
-
-export type ProposalType = keyof typeof PROPOSALS;
 
 export const useCreateProposal = () => {
   const signer = useEthersSigner();
@@ -18,18 +15,23 @@ export const useCreateProposal = () => {
   return useMutation({
     mutationKey: ["createProposal"],
     mutationFn: async (variables: {
-      proposalType: ProposalType;
       description: string;
+      recipient: Address;
+      amount: BigNumber;
     }) => {
       const governor = Governor__factory.connect(
         "0x8340931FAfD164bFe8f802329b50Bd8644BeB52b",
         signer
       );
-      const proposal = PROPOSALS[variables.proposalType];
+      const transferCalldata =
+        ERC20__factory.createInterface().encodeFunctionData("transfer", [
+          variables.recipient,
+          variables.amount,
+        ]);
       const tx = await governor.propose(
-        proposal.targets,
-        proposal.values,
-        proposal.calldatas,
+        PROPOSAL_DATA.targets,
+        PROPOSAL_DATA.values,
+        [transferCalldata],
         variables.description
       );
       const receipt = await tx.wait(2);
@@ -40,6 +42,36 @@ export const useCreateProposal = () => {
         throw new Error("No proposal id found");
       }
       return proposalId;
+    },
+  });
+};
+
+export const useGetProposalState = (props: { proposalId: string }) => {
+  const signer = useEthersSigner();
+  return useQuery({
+    queryKey: ["proposalState", props.proposalId],
+    queryFn: async () => {
+      const governor = Governor__factory.connect(
+        "0x8340931FAfD164bFe8f802329b50Bd8644BeB52b",
+        signer
+      );
+      return await governor.state(props.proposalId);
+    },
+  });
+};
+
+export const useCastVote = (props: { proposalId: string; vote: number }) => {
+  const signer = useEthersSigner();
+  return useMutation({
+    mutationKey: ["castVote", props.proposalId],
+    mutationFn: async () => {
+      const governor = Governor__factory.connect(
+        "0x8340931FAfD164bFe8f802329b50Bd8644BeB52b",
+        signer
+      );
+      const tx = await governor.castVote(props.proposalId, props.vote);
+      const receipt = await tx.wait(2);
+      return receipt;
     },
   });
 };
