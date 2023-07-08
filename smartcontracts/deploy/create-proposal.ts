b8b98073361917ef3c58d02384889cc810104693
seddik11 <seddik.bn@gmail.com>
2023-07-08 10:09:56 +0000

    governor propose

--- smartcontracts/deploy/create-proposal.ts
+++ smartcontracts/deploy/create-proposal.ts
@@ -9,29 +9,34 @@ export default async function (hre: HardhatRuntimeEnvironment) {
   const wallet1 = new Wallet("0xcc1149669c0f288ae228c316d93a8fd39b766a7d5ac3f41054f74e4ee7f1a09c").connect(provider);
   const deployer = new Deployer(hre, wallet1);
 
-  const VotingToken = await hre.artifacts.readArtifact("PeerVoteToken");
-  const deployVotingTokenArtifact = await deployer.loadArtifact("PeerVoteToken");
-  const deployGovernorArtifact = await deployer.loadArtifact("PeerGovernor");
+  const VotingTokenReadArtifact = await hre.artifacts.readArtifact("PeerVoteToken");
+  const GovernorReadArtifact = await hre.artifacts.readArtifact("PeerGovernor");
   
   // deploy contract
-  const votingTokenContract = await deployer.deploy(deployVotingTokenArtifact,[]);
+//   const votingTokenContract = await deployer.deploy(deployVotingTokenArtifact,[]);
 
-  console.log(`// VotingToken contract address ${votingTokenContract.address}`)
+//   console.log(`// VotingToken contract address ${votingTokenContract.address}`)
   
-  const governorContract = await deployer.deploy(deployGovernorArtifact,[votingTokenContract.address]);
+//   const governorContract = await deployer.deploy(deployGovernorArtifact,[votingTokenContract.address]);
 
-  console.log(`// VotingToken contract address ${governorContract.address}`)
+//   console.log(`// VotingToken contract address ${governorContract.address}`)
 
-  const token = await ethers.getContractAt(‘ERC20’, tokenAddress);
+  const votingokenInterface = new ethers.utils.Interface(VotingTokenReadArtifact.abi);
 
   const teamAddress = wallet1.address;
   const grantAmount = 101;
-  const transferCalldata = VotingToken.interface.encodeFunctionData("transfer", [teamAddress, grantAmount]);
+  const transferCalldata = votingokenInterface.encodeFunctionData("transfer", [teamAddress, grantAmount]);
+
+  const governor = new ethers.Contract(
+    "0xe9567E29E59F103Bd89c40DDB1E95603231bed6b",
+    GovernorReadArtifact.abi,
+    wallet1
+  );
 
   await governor.propose(
-    [tokenAddress],
+    ["0x2AE8a8abc98d5b714608225Bac7c68Bc75cDec38"],
     [0],
     [transferCalldata],
-    “Proposal #1: Give grant to team”,
+    "funding for Barcelona public gardens",
   );
 }
