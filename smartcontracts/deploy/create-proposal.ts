69f550d945558feca1ce2345153670b0a30d2595
grmkris <kristjan.grm1@gmail.com>
2023-07-08 14:16:05 +0200

    fix frontend

--- smartcontracts/deploy/create-proposal.ts
+++ smartcontracts/deploy/create-proposal.ts
@@ -2,44 +2,66 @@ import { Wallet, utils, Provider, EIP712Signer, types } from "zksync-web3";
 import * as ethers from "ethers";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
+import { Governor } from "../src/types";
 
 export default async function (hre: HardhatRuntimeEnvironment) {
   const provider = new Provider("https://testnet.era.zksync.dev");
-  
-  const wallet1 = new Wallet("0xcc1149669c0f288ae228c316d93a8fd39b766a7d5ac3f41054f74e4ee7f1a09c").connect(provider);
+
+  const wallet1 = new Wallet(
+    "0xcc1149669c0f288ae228c316d93a8fd39b766a7d5ac3f41054f74e4ee7f1a09c"
+  ).connect(provider);
   const deployer = new Deployer(hre, wallet1);
 
-  const VotingTokenReadArtifact = await hre.artifacts.readArtifact("PeerVoteToken");
+  const VotingTokenReadArtifact = await hre.artifacts.readArtifact(
+    "PeerVoteToken"
+  );
   const GovernorReadArtifact = await hre.artifacts.readArtifact("PeerGovernor");
-  
+
   // deploy contract
-//   const votingTokenContract = await deployer.deploy(deployVotingTokenArtifact,[]);
+  //   const votingTokenContract = await deployer.deploy(deployVotingTokenArtifact,[]);
 
-//   console.log(`// VotingToken contract address ${votingTokenContract.address}`)
-  
-//   const governorContract = await deployer.deploy(deployGovernorArtifact,[votingTokenContract.address]);
+  //   console.log(`// VotingToken contract address ${votingTokenContract.address}`)
 
-//   console.log(`// VotingToken contract address ${governorContract.address}`)
+  //   const governorContract = await deployer.deploy(deployGovernorArtifact,[votingTokenContract.address]);
 
-  const votingokenInterface = new ethers.utils.Interface(VotingTokenReadArtifact.abi);
+  //   console.log(`// VotingToken contract address ${governorContract.address}`)
+
+  const votingokenInterface = new ethers.utils.Interface(
+    VotingTokenReadArtifact.abi
+  );
 
   const teamAddress = wallet1.address;
-  const grantAmount = 153;
-  const transferCalldata = votingokenInterface.encodeFunctionData("transfer", [teamAddress, grantAmount]);
+  const grantAmount = 4204;
+  const transferCalldata = votingokenInterface.encodeFunctionData("transfer", [
+    teamAddress,
+    grantAmount,
+  ]);
 
   const governor = new ethers.Contract(
     "0xe9567E29E59F103Bd89c40DDB1E95603231bed6b",
     GovernorReadArtifact.abi,
     wallet1
-  );
+  ) as Governor;
 
-  const porposalId = await governor.propose(
+  const proposalTx = await governor.propose(
     ["0x2AE8a8abc98d5b714608225Bac7c68Bc75cDec38"],
     [0],
     [transferCalldata],
-    "funding for Barcelona public gardens",
+    "funding for Barcelona public gardens"
   );
 
-  console.log(`// Proposal Id ${porposalId}`);
-  
+  const proposalReceipt = await proposalTx.wait();
+
+  // get emmited event
+  console.log(
+    proposalReceipt.events?.map((e) => ({
+      name: e.event,
+      args: e.args?.map((a) => a.toString()),
+    }))
+  );
+
+  const proposalId = proposalReceipt.events
+    ?.find((e) => e.event === "ProposalCreated")
+    ?.args?.[0].toString();
+  console.log(`Proposal id: ${proposalId}`);
 }
