30704bb4b9fcb25aecc29c30688dd550c7f963ed
grmkris <kristjan.grm1@gmail.com>
2023-07-08 18:07:27 +0200

    graphql

--- frontend/src/hooks/useCreateProposal.tsx
+++ frontend/src/hooks/useCreateProposal.tsx
@@ -1,1 +1,45 @@
-export const useCreateProposal = () => {};
+import { useEthersSigner } from "../useEthersSigner";
+import { useMutation } from "@tanstack/react-query";
+import { Governor__factory } from "../types";
+
+const PROPOSALS = {
+  VoteForSendingMoney: {
+    values: [],
+    calldatas: [],
+    targets: [],
+  },
+};
+
+export type ProposalType = keyof typeof PROPOSALS;
+
+export const useCreateProposal = () => {
+  const signer = useEthersSigner();
+
+  return useMutation({
+    mutationKey: ["createProposal"],
+    mutationFn: async (variables: {
+      proposalType: ProposalType;
+      description: string;
+    }) => {
+      const governor = Governor__factory.connect(
+        "0x8340931FAfD164bFe8f802329b50Bd8644BeB52b",
+        signer
+      );
+      const proposal = PROPOSALS[variables.proposalType];
+      const tx = await governor.propose(
+        proposal.targets,
+        proposal.values,
+        proposal.calldatas,
+        variables.description
+      );
+      const receipt = await tx.wait(2);
+      const proposalId = receipt.events?.find(
+        (event) => event.event === "ProposalCreated"
+      )?.args?.id;
+      if (!proposalId) {
+        throw new Error("No proposal id found");
+      }
+      return proposalId;
+    },
+  });
+};
